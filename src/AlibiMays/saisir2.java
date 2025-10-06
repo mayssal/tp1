86a179fac4c16f455140611da6d7508958d00cbb
Mays Alibi <alibimays5@gmail.com>
2025-10-06 10:50:21 +0100

    Update

--- src/AlibiMays/saisir2.java
+++ src/AlibiMays/saisir2.java
@@ -5,6 +5,9 @@ public class saisir2 {
 	public static void main (String[] args) {
 	Scanner input = new Scanner(System.in);
 	
+	System.out.println("Donner ton prénom:");
+	String nom  = input.nextLine();
+	
 	System.out.print("Entrer le premier nombre:");
 	int a = input.nextInt();
 	
@@ -21,6 +24,7 @@ public class saisir2 {
 	System.out.println("la somme est: " + somme);
 	System.out.println("Le produit est: " + produit);
 	System.out.println("La moyenne est: " + moyenne);
+	System.out.println("Bonjour " + nom + ", la somme est: " + somme +", le produit est: " + produit + " et la moyenne est: " + moyenne);
 	
 	input.close();
 	
